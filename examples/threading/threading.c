01be3f3568dc31c2f984073ad635644d5a8fd496
JoelKellam <joel_kellam@hotmail.co.uk>
2022-09-20 19:07:58 +0100

    assignment 4 part 1 additions

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include<syslog.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -13,7 +14,34 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    // Wait.
+    if (nanosleep(&thread_func_args->wait_to_obtain_ms, NULL) != 0)
+    {
+        syslog(LOG_INFO, "1st nanosleep was successful\n");
+    }
+
+    // Obtain Mutex.
+    if (pthread_mutex_lock(thread_func_args->mutex) != 0) 
+    {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    // Wait.
+    if (nanosleep(&thread_func_args->wait_to_obtain_ms, NULL) != 0)
+    {
+        syslog(LOG_INFO, "2nd nanosleep was successful\n");
+    }
+
+    // Release Mutex.
+    if(pthread_mutex_unlock(thread_func_args->mutex) != 0)
+    {
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +56,20 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    struct thread_data *t_data = (struct thread_data *)malloc(sizeof(struct thread_data));
+    t_data->wait_to_obtain_ms.tv_sec = 0;
+    t_data->wait_to_obtain_ms.tv_nsec = (wait_to_obtain_ms*1000000);
+    t_data->wait_to_release_ms.tv_sec = 0;
+    t_data->wait_to_release_ms.tv_nsec = (wait_to_obtain_ms*1000000);
+    t_data->mutex = mutex;
+
+    // create pthread.
+    if(pthread_create(&t_data->thread, NULL, threadfunc, t_data) == 0)
+    {
+        syslog(LOG_INFO, "pthread created successfully\n");
+    } 
+    *thread = t_data->thread;
+    return true;
 }
 
